6dd4d203ee47653c3866126678cbdff4f392fade
SyaidiPutra <syaidiputra29@gmail.com>
2022-09-19 23:37:23 +0800

    Penyesuaian

--- app/models/pengalamanKerja_model.php
+++ app/models/pengalamanKerja_model.php
@@ -15,80 +15,48 @@ class pengalamanKerja_model
         return $this->db->resultSet();
     }
 
-    public function store1($data)
+    public function store($data, $id)
     {
-        if($data[0]['nama_perusahaan1'] == '') {
-            return 0;
-        } else {
-            $query = "INSERT INTO pengalaman_kerja VALUES ('', :id_calon_karyawan, :nama_perusahaan, :jabatan, :dept, :durasi, :alasan_berhenti, :jobdesc, :gaji_terakhir, :created_at, :updated_at)";
-
-            $this->db->query($query);
-            $this->db->bind('id_calon_karyawan', $data[1]['last_id']);
-            $this->db->bind('nama_perusahaan', $data[0]['nama_perusahaan1']);
-            $this->db->bind('jabatan', $data[0]['jabatan1']);
-            $this->db->bind('dept', $data[0]['dept1']);
-            $this->db->bind('durasi', $data[0]['durasi1']);
-            $this->db->bind('alasan_berhenti', $data[0]['alasan_berhenti1']);
-            $this->db->bind('jobdesc', $data[0]['jobdesc1']);
-            $this->db->bind('gaji_terakhir', $data[0]['gaji_terakhir1']);
-            $this->db->bind('created_at', date('Y-m-d h:i:s'));
-            $this->db->bind('updated_at', null);
-
-            $this->db->execute();
-
-            return $this->db->rowCount();
-        }
+       foreach($data['nama_perusahaan'] as $key => $val){
+            if($val == ''){
+                continue;
+            }
+            $susun = [
+                'id_calon_karyawan' => $id['last_id'],
+                'nama_perusahaan' => $data['nama_perusahaan'],
+                'jabatan' => $data['jabatan'],
+                'dept' => $data['dept'],
+                'durasi' => $data['durasi'],
+                'alasan_berhenti' => $data['alasan_berhenti'],
+                'jobdesc' => $data['jobdesc'],
+                'gaji_terakhir' => $data['gaji_terakhir'],
+            ];
+            $respon[] = $this->runStore($susun);
+       }
+       return $respon;
     }
 
-    public function store2($data)
+    private function runStore($data)
     {
-        if($data[0]['nama_perusahaan2'] == '') {
-            return 0;
-        } else {
-            $query = "INSERT INTO pengalaman_kerja VALUES ('', :id_calon_karyawan, :nama_perusahaan, :jabatan, :dept, :durasi, :alasan_berhenti, :jobdesc, :gaji_terakhir, :created_at, :updated_at)";
+        $query = "INSERT INTO pengalaman_kerja VALUES ('', :id_calon_karyawan, :nama_perusahaan, :jabatan, :dept, :durasi, :alasan_berhenti, :jobdesc, :gaji_terakhir, :created_at, :updated_at)";
 
-            $this->db->query($query);
-            $this->db->bind('id_calon_karyawan', $data[1]['last_id']);
-            $this->db->bind('nama_perusahaan', $data[0]['nama_perusahaan2']);
-            $this->db->bind('jabatan', $data[0]['jabatan2']);
-            $this->db->bind('dept', $data[0]['dept2']);
-            $this->db->bind('durasi', $data[0]['durasi2']);
-            $this->db->bind('alasan_berhenti', $data[0]['alasan_berhenti2']);
-            $this->db->bind('jobdesc', $data[0]['jobdesc2']);
-            $this->db->bind('gaji_terakhir', $data[0]['gaji_terakhir2']);
-            $this->db->bind('created_at', date('Y-m-d h:i:s'));
-            $this->db->bind('updated_at', null);
+        $this->db->query($query);
+        $this->db->bind('id_calon_karyawan', $data['id_calon_karyawan']);
+        $this->db->bind('nama_perusahaan', $data['nama_perusahaan']);
+        $this->db->bind('jabatan', $data['jabatan']);
+        $this->db->bind('dept', $data['dept']);
+        $this->db->bind('durasi', $data['durasi']);
+        $this->db->bind('alasan_berhenti', $data['alasan_berhenti']);
+        $this->db->bind('jobdesc', $data['jobdesc']);
+        $this->db->bind('gaji_terakhir', $data['gaji_terakhir']);
+        $this->db->bind('created_at', date('Y-m-d h:i:s'));
+        $this->db->bind('updated_at', null);
 
-            $this->db->execute();
+        $this->db->execute();
 
-            return $this->db->rowCount();
-        }
+        return $this->db->rowCount();
     }
 
-    public function store3($data)
-    {
-        if($data[0]['nama_perusahaan3'] == '') {
-            return 0;
-        } else {
-            $query = "INSERT INTO pengalaman_kerja VALUES ('', :id_calon_karyawan, :nama_perusahaan, :jabatan, :dept, :durasi, :alasan_berhenti, :jobdesc, :gaji_terakhir, :created_at, :updated_at)";
-
-            $this->db->query($query);
-            $this->db->bind('id_calon_karyawan', $data[1]['last_id']);
-            $this->db->bind('nama_perusahaan', $data[0]['nama_perusahaan3']);
-            $this->db->bind('jabatan', $data[0]['jabatan3']);
-            $this->db->bind('dept', $data[0]['dept3']);
-            $this->db->bind('durasi', $data[0]['durasi3']);
-            $this->db->bind('alasan_berhenti', $data[0]['alasan_berhenti3']);
-            $this->db->bind('jobdesc', $data[0]['jobdesc3']);
-            $this->db->bind('gaji_terakhir', $data[0]['gaji_terakhir3']);
-            $this->db->bind('created_at', date('Y-m-d h:i:s'));
-            $this->db->bind('updated_at', null);
-
-            $this->db->execute();
-
-            return $this->db->rowCount();
-        }
-    }
 
     public function update($data)
     {
--- app/models/riwayatPendidikan_model.php
+++ app/models/riwayatPendidikan_model.php
@@ -15,72 +15,44 @@ class riwayatPendidikan_model
         $this->db->bind('id', $id);
         return $this->db->resultSet();
     }
+    public function store($data, $id)
+    {
+        foreach($data['nama_lembaga'] as $key => $val){
+            if($val == ''){
+                continue;
+            }
+            $rekap = [
+                'id_calon_karyawan' => $id['last_id'],
+                'jenis_pendidikan' => $data['jenis_pendidikan'][$key],
+                'jenjang_pendidikan' => $data['jenjang_pendidikan'][$key],
+                'program_keahlian' => $data['program_keahlian'][$key],
+                'nama_lembaga' => $data['nama_lembaga'][$key],
+                'alamat_lembaga' => $data['alamat_lembaga'][$key],
+                'berijazah' => $data['berijazah'][$key]
+            ];
+           $respone[] = $this->RunStore($rekap);
+        }
+        return $respone;
+    }
 
-    public function store1($data)
+    private function RunStore($data)
     {
         $query = "INSERT INTO riwayat_pendidikan VALUES ('', :id_calon_karyawan, :jenis_pendidikan, :jenjang_pendidikan, :program_keahlian, :nama_lembaga, :alamat_lembaga, :berijazah, :created_at, :updated_at)";
 
         $this->db->query($query);
-        $this->db->bind('id_calon_karyawan', $data[1]['last_id']);
-        $this->db->bind('jenis_pendidikan', $data[0]['jenis_pendidikan1']);
-        $this->db->bind('jenjang_pendidikan', $data[0]['jenjang_pendidikan1']);
-        $this->db->bind('program_keahlian', $data[0]['program_keahlian1']);
-        $this->db->bind('nama_lembaga', $data[0]['nama_lembaga1']);
-        $this->db->bind('alamat_lembaga', $data[0]['alamat_lembaga1']);
-        $this->db->bind('berijazah', $data[0]['berijazah1']);
+        $this->db->bind('id_calon_karyawan', $data['id_calon_karyawan']);
+        $this->db->bind('jenis_pendidikan', $data['jenis_pendidikan']);
+        $this->db->bind('jenjang_pendidikan', $data['jenjang_pendidikan']);
+        $this->db->bind('program_keahlian', $data['program_keahlian']);
+        $this->db->bind('nama_lembaga', $data['nama_lembaga']);
+        $this->db->bind('alamat_lembaga', $data['alamat_lembaga']);
+        $this->db->bind('berijazah', $data['berijazah']);
         $this->db->bind('created_at', date('Y-m-d h:i:s'));
         $this->db->bind('updated_at', null);
 
         $this->db->execute();
 
         return $this->db->rowCount();
-    }
-
-    public function store2($data)
-    {
-        if($data[0]['jenis_pendidikan2'] == '') {
-            return 0;
-        } else {
-            $query = "INSERT INTO riwayat_pendidikan VALUES ('', :id_calon_karyawan, :jenis_pendidikan, :jenjang_pendidikan, :program_keahlian, :nama_lembaga, :alamat_lembaga, :berijazah, :created_at, :updated_at)";
-            $this->db->query($query);
-            $this->db->bind('id_calon_karyawan', $data[1]['last_id']);
-            $this->db->bind('jenis_pendidikan', $data[0]['jenis_pendidikan2']);
-            $this->db->bind('jenjang_pendidikan', $data[0]['jenjang_pendidikan2']);
-            $this->db->bind('program_keahlian', $data[0]['program_keahlian2']);
-            $this->db->bind('nama_lembaga', $data[0]['nama_lembaga2']);
-            $this->db->bind('alamat_lembaga', $data[0]['alamat_lembaga2']);
-            $this->db->bind('berijazah', $data[0]['berijazah2']);
-            $this->db->bind('created_at', date('Y-m-d h:i:s'));
-            $this->db->bind('updated_at', null);
-
-            $this->db->execute();
-
-            return $this->db->rowCount();
-        } 
-    }
-
-    public function store3($data)
-    {
-        if($data[0]['jenis_pendidikan3'] == '') {
-            return 0;
-        } else {
-            $query = "INSERT INTO riwayat_pendidikan VALUES ('', :id_calon_karyawan, :jenis_pendidikan, :jenjang_pendidikan, :program_keahlian, :nama_lembaga, :alamat_lembaga, :berijazah, :created_at, :updated_at)";
-
-            $this->db->query($query);
-            $this->db->bind('id_calon_karyawan', $data[1]['last_id']);
-            $this->db->bind('jenis_pendidikan', $data[0]['jenis_pendidikan3']);
-            $this->db->bind('jenjang_pendidikan', $data[0]['jenjang_pendidikan3']);
-            $this->db->bind('program_keahlian', $data[0]['program_keahlian3']);
-            $this->db->bind('nama_lembaga', $data[0]['nama_lembaga3']);
-            $this->db->bind('alamat_lembaga', $data[0]['alamat_lembaga3']);
-            $this->db->bind('berijazah', $data[0]['berijazah3']);
-            $this->db->bind('created_at', date('Y-m-d h:i:s'));
-            $this->db->bind('updated_at', null);
-
-            $this->db->execute();
-
-            return $this->db->rowCount();
-        }
     }
 
     public function update($data)
