976f53da31083b6a8ef97440d7475f0688215d41
SyaidiPutra <syaidiputra29@gmail.com>
2022-09-19 23:37:02 +0800

    Perampingan

--- app/controllers/calonKaryawan.php
+++ app/controllers/calonKaryawan.php
@@ -79,48 +79,17 @@ class calonKaryawan extends Controller
 
     public function store()
     {
+        // die(var_dump($_POST['pengalaman']));
         $storeBiodata = $this->model('calonKaryawan_model')->store($_POST);
         $getLastId = $this->model('calonKaryawan_model')->getLastId();
 
         if($storeBiodata > 0) {
-            $storeRiwayatPendidikan1 = $this->model('riwayatPendidikan_model')->store1([$_POST, $getLastId]);
-            if($storeRiwayatPendidikan1 > 0) {
-                $storeRiwayatPendidikan2 = $this->model('riwayatPendidikan_model')->store2([$_POST, $getLastId]);
-                if($storeRiwayatPendidikan2 >= 0) {
-                    $storeRiwayatPendidikan3 = $this->model('riwayatPendidikan_model')->store3([$_POST, $getLastId]);
-                    if($storeRiwayatPendidikan3 >= 0) {
-                        $pengalamanKerja1 = $this->model('pengalamanKerja_model')->store1([$_POST, $getLastId]);
-                        if($pengalamanKerja1 >= 0) {
-                            $pengalamanKerja2 = $this->model('pengalamanKerja_model')->store2([$_POST, $getLastId]);
-                            if($pengalamanKerja2 >= 0) {
-                                $pengalamanKerja3 = $this->model('pengalamanKerja_model')->store3([$_POST, $getLastId]);
-                                if($pengalamanKerja3 >= 0) {
-                                    Flasher::setFlash('success', 'Berhasil menambahkan biodata, riwayat pendidikan & pengalaman kerja ' . $_POST['nama_depan'], '<i class="bi bi-check2-circle"></i>');
-                                    header('Location: ' . BASEURL . '/calonKaryawan');
-                                } else {
-                                    Flasher::setFlash('danger', 'Gagal menambahkan pengalaman kerja #3 ' . $_POST['nama_depan'], ' !');
-                                    header('Location: ' . BASEURL . '/calonKaryawan');
-                                }
-                            } else {
-                                Flasher::setFlash('danger', 'Gagal menambahkan pengalaman kerja #2 ' . $_POST['nama_depan'], ' !');
-                                header('Location: ' . BASEURL . '/calonKaryawan');
-                            }
-                        } else {
-                            Flasher::setFlash('danger', 'Gagal menambahkan pengalaman kerja #1 ' . $_POST['nama_depan'], ' !');
-                            header('Location: ' . BASEURL . '/calonKaryawan');
-                        }
-                    } else {
-                        Flasher::setFlash('danger', 'Gagal menambahkan pendidikan non formal #2 ' . $_POST['nama_depan'], ' !');
-                        header('Location: ' . BASEURL . '/calonKaryawan');
-                    }
-                } else {
-                    Flasher::setFlash('danger', 'Gagal menambahkan pendidikan non formal #1 ' . $_POST['nama_depan'], ' !');
-                    header('Location: ' . BASEURL . '/calonKaryawan');
-                }
-            } else {
-                Flasher::setFlash('danger', 'Gagal menambahkan riwayat pendidikan formal ' . $_POST['nama_depan'], ' !');
-                header('Location: ' . BASEURL . '/calonKaryawan');
-            }
+
+            $this->model('riwayatPendidikan_model')->store($_POST['pendidikan'], $getLastId);
+            $this->model('pengalamanKerja_model')->store($_POST['pendidikan'], $getLastId);
+            Flasher::setFlash('success', 'Berhasil menambahkan biodata, riwayat pendidikan & pengalaman kerja ' . $_POST['nama_depan'], '<i class="bi bi-check2-circle"></i>');
+            header('Location: ' . BASEURL . '/calonKaryawan');
+            
         } else {
             Flasher::setFlash('danger', 'Gagal menambahkan calon karyawan ' . $_POST['nama_depan'], ' !');
             header('Location: ' . BASEURL . '/calonKaryawan');
@@ -175,5 +144,13 @@ class calonKaryawan extends Controller
             Flasher::setFlash('danger', 'Gagal menghapus pengalaman kerja ' . $_POST['nama_depan'], ' !');
             header('Location: ' . BASEURL . '/calonKaryawan');
         }
+    }
+
+    public function tes()
+    {
+        $this->view('test');
+
+        var_dump($_POST);
+
     }
 }
